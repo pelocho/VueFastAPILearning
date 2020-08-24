64b384d4956f8f46eaf8c69966179710ccfffb4f
Oscar Morente <oscar.morente@ebury.com>
2020-08-24 17:43:57 +0200

    Extracted all products card to the component and add event to show the sidevar inside the product card component.

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,22 +1,14 @@
 <template>
   <div>
     <div class="mt-20 flex-container-center">
-      <router-link :to="{name: 'create-product'}" v-on:click.native.prevent="showSidebar" class="btn btn-success">New Product</router-link>
+      <router-link :to="{name: 'create-product'}" v-on:click.native.prevent="showSidebar" class="btn btn-success">New
+        Product
+      </router-link>
     </div>
-      <div v-for="product in products" :key="product.id_" class="flex-container-center mt-20">
-        <router-link class="product-link" :to="{name: 'edit-product', params: {id: product.id_}}" v-on:click.native.prevent="showSidebar">
-          <ProductCard>
-            <h4 slot="name">{{ product.name }}</h4>
-            <h5 slot="status">{{ product.status }}</h5>
-            <h5 slot="price">{{ product.price }}</h5>
-            <h5 slot="location">{{ product.location }}</h5>
-          </ProductCard>
-          <button class="btn btn-danger ml-20" v-on:click="deleteProduct(product.id_)">Delete</button>
-        </router-link>
-      </div>
-      <Sidebar v-if="this.showPanel">
-        <router-view></router-view>
-      </Sidebar>
+    <ProductCard v-for="product in products" :key="product.id_" :product="product" class="flex-container-center mt-20" @showEditSidebar="showSidebar"></ProductCard>
+    <Sidebar v-if="this.showPanel">
+      <router-view></router-view>
+    </Sidebar>
   </div>
 </template>
 
@@ -46,22 +38,9 @@
                 this.axios.get('http://localhost:8000/products')
                     .then(response => (this.products = response.data))
             },
-            deleteProduct(id) {
-                let url = `http://localhost:8000/products/${id}`;
-                this.axios.delete(url)
-                    .then(this.$router.go());
-            },
             showSidebar() {
                 this.showPanel =  !this.showPanel
             }
         }
     };
 </script>
-
-<style scoped>
-.product-link {
-  color: black;
-  text-decoration: none;
-  font-weight: 100;
-}
-</style>
--- app/frontend/src/components/ProductCard.vue
+++ app/frontend/src/components/ProductCard.vue
@@ -1,27 +1,44 @@
 <template>
-  <div class="product-card shadow">
-    <div class="row ml-5">
-      <h4 class="mr-20">Name:</h4>
-      <slot name="name"></slot>
+  <router-link class="product-link" :to="{name: 'edit-product', params: {id: product.id_}}"
+               v-on:click.native.prevent="showSidebar">
+    <div class="product-card shadow">
+      <div class="row ml-5">
+        <h4 class="mr-20">Name:</h4>
+        <h4>{{ product.name }}</h4>
+      </div>
+      <div class="row ml-5">
+        <h5 class="mr-20">Status</h5>
+        <h5>{{ product.status }}</h5>
+      </div>
+      <div class="row ml-5">
+        <h5 class="mr-20">Price</h5>
+        <h5>{{ product.price }}</h5>
+      </div>
+      <div class="row ml-5">
+        <h5 class="mr-20">Location</h5>
+        <h5>{{ product.location }}</h5>
+      </div>
+      <button class="btn btn-danger mt-20 ml-5" v-on:click="deleteProduct(product.id_)">Delete</button>
     </div>
-    <div class="row ml-5">
-      <h5 class="mr-20">Status</h5>
-      <slot name="status"></slot>
-    </div>
-    <div class="row ml-5">
-      <h5 class="mr-20">Price</h5>
-      <slot name="price"></slot>
-    </div>
-    <div class="row ml-5">
-      <h5 class="mr-20">Location</h5>
-      <slot name="location"></slot>
-    </div>
-  </div>
+  </router-link>
 </template>
 
 <script>
     export default {
-        name: "ProductCard"
+        name: "ProductCard",
+        props: {
+            product: Object
+        },
+        methods: {
+            deleteProduct(id) {
+                let url = `http://localhost:8000/products/${id}`;
+                this.axios.delete(url)
+                    .then(this.$router.go());
+            },
+            showSidebar() {
+                this.$emit('showEditSidebar')
+            }
+        }
     }
 </script>
 
@@ -37,5 +54,11 @@
   .product-card:hover {
     transform: scale(1.01);
     box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+  }
+
+  .product-link {
+    color: black;
+    text-decoration: none;
+    font-weight: 100;
   }
 </style>
