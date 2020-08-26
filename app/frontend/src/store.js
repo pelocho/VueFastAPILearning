ff9d82b5034a69456e552c238bd3456939a36832
Oscar Morente <oscar.morente@ebury.com>
2020-08-26 13:21:05 +0200

    Solved problem with status of the sidevar

--- app/frontend/src/store.js
+++ app/frontend/src/store.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        sidevarOpen: true,
+        sidevarOpen: false,
     },
     mutations: {
         SHOW_SIDEVAR(state) {
